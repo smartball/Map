73e8d2516e15128beaff8137f7f8ebcaf4e5c4f3
smartball <ball-smartball@hotmail.com>
2017-12-09 01:47:16 +0700

    version1.2

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -4,30 +4,68 @@ using Android.OS;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using System;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+using Map_vTest.Droid.Model;
+using System.Threading.Tasks;
+using Android.Views;
+using Firebase.Xamarin.Database;
+using Firebase.Xamarin.Database.Query;
+using static Android.Gms.Maps.GoogleMap;
 
 namespace Map_vTest.Droid
 {
-    [Activity(Label = "Map_vTest", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity,IOnMapReadyCallback
+    [Activity(Label = "Map_vTest", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback, IInfoWindowAdapter, IOnInfoWindowClickListener
     {
-        
+        private EditText input_name, input_email;
+        private ListView list_data;
 
+        private List<Account> list_users = new List<Account>();
+        private ListViewAdapter adapter; 
+        private Account selectedAccount;
 
-        public void OnMapReady(GoogleMap googleMap)
+        private const string FirebaseURL = "https://xamarin-74fb6.firebaseio.com/";
+
+        double posi_lat = 13.7298956 ;
+        double posi_lng = 100.7771276;
+
+        void IOnMapReadyCallback.OnMapReady(GoogleMap googleMap)
         {
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.SetPosition(new LatLng(16.03, 108));
-            markerOptions.SetTitle("My Position");
-            googleMap.AddMarker(markerOptions);
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate
+            {
+                button.Text = $"{posi_lat},{posi_lng}";
+            };
+
+            if (Math.Abs(posi_lat) > 0)
+            {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.SetPosition(new LatLng(posi_lat, posi_lng));
+
+                markerOptions.SetTitle("My Position");
+
+                googleMap.AddMarker(markerOptions);
+            }
+
+            googleMap.SetInfoWindowAdapter(this);
+            googleMap.SetOnInfoWindowClickListener(this);
 
             //optional
             googleMap.UiSettings.ZoomControlsEnabled = true;
             googleMap.UiSettings.CompassEnabled = true;
-            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+            if(Math.Abs(posi_lat) > 0)
+            {
+                googleMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(new LatLng(posi_lat, posi_lng), 17.0f));
+            }
+            else{
+                googleMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(new LatLng(13.7448301, 100.7226308), 12.0f));
+            }
+
 
         }
 
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
@@ -36,9 +74,126 @@ namespace Map_vTest.Droid
             MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
             mapFragment.GetMapAsync(this );
 
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            toolbar.Title = "Firebase Demo";
+            SetSupportActionBar(toolbar); 
+
+            input_name = FindViewById<EditText>(Resource.Id.name);
+            input_email = FindViewById<EditText>(Resource.Id.email);
+            list_data = FindViewById<ListView>(Resource.Id.list_data);
+            list_data.ItemClick += (s, e) => {
+                Account acc = list_users[e.Position];
+                selectedAccount = acc;
+                input_name.Text = acc.name;
+                input_email.Text = acc.email;
+            };
+
+            await LoadDataAsync();
+
         }
 
+        private async Task LoadDataAsync()
+        {
+            list_data.Visibility = ViewStates.Invisible;
 
+            var firebase = new FirebaseClient(FirebaseURL);
+            var items = await firebase
+                .Child("users")
+                .OnceAsync<Account>();
+            list_users.Clear();
+            adapter = null;
+            foreach (var item in items)
+            {
+                Account acc = new Account();
+                acc.uid = item.Key;
+                acc.name = item.Object.name;
+                acc.email = item.Object.email;
+
+                list_users.Add(acc);
+            }
+            adapter = new ListViewAdapter(this, list_users);
+            adapter.NotifyDataSetChanged();
+            list_data.Adapter = adapter;
+            list_data.Visibility = ViewStates.Visible;
+
+        }
+
+        public override bool OnCreateOptionsMenu(Android.Views.IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.menu_add)
+            {
+                CreateUser();
+            }
+            else if (id == Resource.Id.menu_save) // Update
+            {
+                UpdateUser(selectedAccount.uid, input_name.Text, input_email.Text);
+            }
+            else if (id == Resource.Id.menu_remove)
+            {
+                DeleteUser(selectedAccount.uid);
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private async void DeleteUser(string uid)
+        {
+            var firebase = new FirebaseClient(FirebaseURL);
+            await firebase.Child("users").Child(uid).DeleteAsync();
+            await LoadDataAsync();
+        }
+
+        private async void UpdateUser(string uid, string name, string email)
+        {
+            var firebase = new FirebaseClient(FirebaseURL);
+            await firebase.Child("users").Child(uid).Child("name").PutAsync(name);
+            await firebase.Child("users").Child(uid).Child("email").PutAsync(email);
+
+            await LoadDataAsync();
+        }
+
+        private async void CreateUser()
+        {
+            Account user = new Account();
+            user.uid = String.Empty;
+            user.name = input_name.Text;
+            user.email = input_email.Text;
+
+            var firebase = new FirebaseClient(FirebaseURL);
+
+            //Add item
+            var item = await firebase.Child("users").PostAsync<Account>(user);
+
+            await LoadDataAsync();
+        }
+
+        public View GetInfoContents(Marker marker)
+        {
+            return null;
+        }
+
+        public View GetInfoWindow(Marker marker)
+        {
+            View view = LayoutInflater.Inflate(Resource.Layout.info_window, null, true);
+            view.FindViewById<TextView>(Resource.Id.txtName).Text = "Xamarin";
+            view.FindViewById<TextView>(Resource.Id.txtAddress).Text = "Ladkrabang";
+            view.FindViewById<TextView>(Resource.Id.txtHours).Text = "8.00 AM";
+
+
+            return view;
+        }
+
+        public void OnInfoWindowClick(Marker marker)
+        {
+            Console.WriteLine("Info Window has been Click!");
+        }
     }
 }
+
 
--- Droid/obj/Debug/android/src/md5b1ab7eae3f1a9fc3be67ab020735c751/MainActivity.java
+++ Droid/obj/Debug/android/src/md5b1ab7eae3f1a9fc3be67ab020735c751/MainActivity.java
@@ -2,17 +2,24 @@ package md5b1ab7eae3f1a9fc3be67ab020735c751;
 
 
 public class MainActivity
-	extends android.app.Activity
+	extends android.support.v7.app.AppCompatActivity
 	implements
 		mono.android.IGCUserPeer,
-		com.google.android.gms.maps.OnMapReadyCallback
+		com.google.android.gms.maps.OnMapReadyCallback,
+		com.google.android.gms.maps.GoogleMap.InfoWindowAdapter,
+		com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener
 {
 /** @hide */
 	public static final String __md_methods;
 	static {
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateOptionsMenu:(Landroid/view/Menu;)Z:GetOnCreateOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
 			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_getInfoContents:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoContents_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_getInfoWindow:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoWindow_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_onInfoWindowClick:(Lcom/google/android/gms/maps/model/Marker;)V:GetOnInfoWindowClick_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IOnInfoWindowClickListenerInvoker, Xamarin.GooglePlayServices.Maps\n" +
 			"";
 		mono.android.Runtime.register ("Map_vTest.Droid.MainActivity, Map_vTest.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
 	}
@@ -34,12 +41,52 @@ public class MainActivity
 	private native void n_onCreate (android.os.Bundle p0);
 
 
+	public boolean onCreateOptionsMenu (android.view.Menu p0)
+	{
+		return n_onCreateOptionsMenu (p0);
+	}
+
+	private native boolean n_onCreateOptionsMenu (android.view.Menu p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+
 	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
 	{
 		n_onMapReady (p0);
 	}
 
 	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+
+	public android.view.View getInfoContents (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoContents (p0);
+	}
+
+	private native android.view.View n_getInfoContents (com.google.android.gms.maps.model.Marker p0);
+
+
+	public android.view.View getInfoWindow (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoWindow (p0);
+	}
+
+	private native android.view.View n_getInfoWindow (com.google.android.gms.maps.model.Marker p0);
+
+
+	public void onInfoWindowClick (com.google.android.gms.maps.model.Marker p0)
+	{
+		n_onInfoWindowClick (p0);
+	}
+
+	private native void n_onInfoWindowClick (com.google.android.gms.maps.model.Marker p0);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
